405a8f96a7dd561a39cc1a70d2a3b48988eb3967
Diogo <Diogo@172.30.57.61>
2014-05-14 16:24:45 +0100

    111

---
+++ src/Email.h
@@ -0,0 +1,17 @@
+/*
+ * Email.h
+ *
+ *  Created on: 14/05/2014
+ *      Author: Diogo
+ */
+
+#ifndef EMAIL_H_
+#define EMAIL_H_
+
+class Email {
+public:
+	Email();
+	virtual ~Email();
+};
+
+#endif /* EMAIL_H_ */
